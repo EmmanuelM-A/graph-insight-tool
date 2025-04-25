ca29615c5a83bd11595ed59d6d16029b280e8219
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-04-25 14:45:52 +0100

    Improved analyzer

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -20,8 +20,8 @@ def analyze(df):
 
         is_email = col_lower in ['email', 'email_address']
         is_ip = 'ip' in col_lower and 'address' in col_lower
-        is_name = 'name' in col_lower or col_lower in ['first_name', 'last_name', 'firstname',
-                                                       'lastname', 'first', 'last', 'family_name', 'surname']
+        is_name = col_lower in ['first_name', 'last_name', 'firstname', 'lastname', 'first', 'last', 'family_name',
+                                'surname']
         is_text_heavy = col_data.map(lambda x: isinstance(x, str) and len(x) > 30).mean() > 0.5
 
         if is_id_like:
@@ -34,28 +34,17 @@ def analyze(df):
             column_info[col] = 'datetime'
         elif col_data.dtype == 'object':
             try:
-                parsed = pd.to_datetime(col_data, errors='raise', infer_datetime_format=True, dayfirst=True)
+                parsed = pd.to_datetime(col_data, errors='raise', utc=True)
                 df[col] = parsed
                 column_info[col] = 'datetime'
             except ValueError:
-                if unique_vals < 20 and not is_name:
+                if unique_vals < 100 and unique_ratio < 0.5:
                     column_info[col] = 'categorical'
                 elif is_text_heavy:
                     column_info[col] = 'text'
                 else:
-                    column_info[col] = 'other'
+                    column_info[col] = 'label'
         else:
-            column_info[col] = 'other'
+            column_info[col] = 'label'
 
     return column_info
-
-
-'''
-elif col_data.dtype == 'object':
-    try:
-        col_data = pd.to_datetime(col_data, errors='raise', infer_datetime_format=True)
-        if pd.api.types.is_datetime64_any_dtype(col_data):
-            column_info[col] = 'datetime'
-    except ValueError:
-        pass  # Not a datetime column
-'''
