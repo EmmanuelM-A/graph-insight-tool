2742b7032fd5b4110105895e51a55bda0b88cf11
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-22 22:38:43 +0100

    The converted dataframe is now returned upon upload data route. The dataframe returned can either be a quick preview of the entire dataset depending on if a flag is set.

--- src/controllers/upload_controller.py
+++ src/controllers/upload_controller.py
@@ -15,7 +15,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def process_upload_request(request):
+def process_upload_request(request, show_all_data=False):
     # Check if the request is a POST request
     if request.method != "POST":
         logger.error("Invalid POST request!")
@@ -74,6 +74,5 @@ def process_upload_request(request):
     # Return basic metadata
     return jsonify({
         "message": message,
-        "columns": data.columns.tolist(),
-        "shape": data.shape
+        "data": data if show_all_data else data.heaad(10),
     }), HTTP_OK
