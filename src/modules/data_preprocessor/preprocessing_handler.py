6f3ab21eacfa73561bfcaf1052c9c72d642d097a
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-06-22 15:34:34 +0100

    Implementing the base class for data preprocessing.

--- src/modules/data_preprocessor/preprocessing_handler.py
+++ src/modules/data_preprocessor/preprocessing_handler.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from abc import ABC, abstractmethod
 
 class PreprocessingHandler:
     """
@@ -13,4 +14,51 @@ class PreprocessingHandler:
         self.__normalizers = []
 
     def preprocess_data(self) -> pd.DataFrame:
+        pass
+
+
+class DataPreprocessor(ABC):
+    """
+    Abstract base class for data preprocessing.
+    """
+
+    def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        if not self.run_data_checks(data):
+            raise ValueError("Data does not meet the required conditions.")
+            
+        treated_data = self.run_data_treatments(data)
+
+        normalized_data = self.normalize_data(treated_data)
+
+        encoded_data = self.encode_data(normalized_data)
+
+        return encoded_data
+
+    def run_data_treatments(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Get the list of treatments applied during preprocessing.
+
+        :return: List of treatments.
+        """
+        pass
+
+    def run_data_checks(self, data: pd.DataFrame) -> bool:
+        """
+        Run data checks to ensure the data meets the required conditions.
+
+        :param data: DataFrame to check.
+        :return: True if data passes all checks, False otherwise.
+        """
+        pass
+
+    def encode_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def normalize_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Run normalizers on the data.
+
+        :param data: DataFrame to normalize.
+        :return: Normalized DataFrame.
+        """
         pass
